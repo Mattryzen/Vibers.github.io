1004300e1e9a091a9621334553dc42b7f27def8c
Mattryzen <166523057+Mattryzen@users.noreply.github.com>
2024-08-28 21:50:22 +0200

    Alpha V 0.4 (added js)

---
+++ scripts.js
@@ -0,0 +1,34 @@
+// List of music tracks
+const tracks = [
+    { title: "A Song", url: "https://www.dropbox.com/s/example1.mp3?dl=1" },
+    { title: "B Song", url: "https://www.dropbox.com/s/example2.mp3?dl=1" },
+    { title: "Hall of Fame - The Script", url: "https://www.dropbox.com/scl/fi/20cx7ztdcccnvz3svu3av/The-Script-Hall-of-Fame.mp3?rlkey=up2bup4fywiv9m9e470y535kr&st=vl2flm0q&dl=1" },
+    { title: "Kompa - Rarin", url: "https://www.dropbox.com/scl/fi/2zvn8e9ysgtz5ro47oyyy/Rarin-Kompa.mp3?dl=1" },
+    // Add more tracks here
+];
+
+// Function to sort tracks alphabetically by title
+tracks.sort((a, b) => a.title.localeCompare(b.title));
+
+// Function to generate the HTML for each track and add to the page
+function displayTracks() {
+    const musicList = document.getElementById("music-list");
+
+    tracks.forEach(track => {
+        const listItem = document.createElement("li");
+
+        const trackTitle = document.createElement("h3");
+        trackTitle.textContent = track.title;
+
+        const audioPlayer = document.createElement("audio");
+        audioPlayer.controls = true;
+        audioPlayer.src = track.url;
+
+        listItem.appendChild(trackTitle);
+        listItem.appendChild(audioPlayer);
+        musicList.appendChild(listItem);
+    });
+}
+
+// Call the function to display tracks
+displayTracks();
