a26f1c45d4f968e7f63740651c3c7ea8fa0b942b
Mattryzen <166523057+Mattryzen@users.noreply.github.com>
2024-08-28 22:24:24 +0200

    Alpha V0.6

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 // List of music tracks
 const tracks = [
-    { title: "A Song", url: "https://www.dropbox.com/s/example1.mp3?dl=1" },
-    { title: "B Song", url: "https://www.dropbox.com/s/example2.mp3?dl=1" },
-    { title: "Hall of Fame - The Script", url: "https://www.dropbox.com/scl/fi/20cx7ztdcccnvz3svu3av/The-Script-Hall-of-Fame.mp3?rlkey=up2bup4fywiv9m9e470y535kr&st=vl2flm0q&dl=1" },
-    { title: "Kompa - Rarin", url: "https://www.dropbox.com/scl/fi/2zvn8e9ysgtz5ro47oyyy/Rarin-Kompa.mp3?rlkey=335nt137qrpsanoavkl77etos&st=5pt7k619&dl=1" },
+    { 
+        title: "Hall of Fame", 
+        artist: "The Script", 
+        duration: "1:47",
+        url: "https://www.dropbox.com/scl/fi/20cx7ztdcccnvz3svu3av/The-Script-Hall-of-Fame.mp3?rlkey=up2bup4fywiv9m9e470y535kr&st=vl2flm0q&dl=1" 
+    },
+    { 
+        title: "Kompa", 
+        artist: "Rarin", 
+        duration: "2:21",  
+        url: "https://www.dropbox.com/scl/fi/2zvn8e9ysgtz5ro47oyyy/Rarin-Kompa.mp3?rlkey=335nt137qrpsanoavkl77etos&st=5pt7k619&dl=1" 
+    },
+   
     // Add more tracks here
 ];
 
@@ -17,16 +26,35 @@ function displayTracks(filteredTracks = tracks) {
 
     filteredTracks.forEach(track => {
         const listItem = document.createElement("li");
+        listItem.className = "track-item";
+
+        const trackInfo = document.createElement("div");
+        trackInfo.className = "track-info";
 
         const trackTitle = document.createElement("h3");
         trackTitle.textContent = track.title;
+        trackTitle.className = "track-title";
+
+        const trackArtist = document.createElement("p");
+        trackArtist.textContent = track.artist;
+        trackArtist.className = "track-artist";
+
+        const trackDuration = document.createElement("span");
+        trackDuration.textContent = track.duration;
+        trackDuration.className = "track-duration";
 
         const audioPlayer = document.createElement("audio");
         audioPlayer.controls = true;
         audioPlayer.src = track.url;
+        audioPlayer.className = "audio-player";
 
-        listItem.appendChild(trackTitle);
+        trackInfo.appendChild(trackTitle);
+        trackInfo.appendChild(trackArtist);
+
+        listItem.appendChild(trackInfo);
+        listItem.appendChild(trackDuration);
         listItem.appendChild(audioPlayer);
+
         musicList.appendChild(listItem);
     });
 }
